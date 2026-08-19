5b5c3ba4f303d600777258ad62c352e63509ccb6
Donat Kabashi <donatkabashi@gmail.com>
2026-08-19 13:09:35 +0200

    Initialize the project, add app icon and git ignore

---
+++ Kroky/KrokyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrokyApp.swift
+//  Kroky
+//
+//  Created by Donat Kabashi on 8/19/26.
+//
+
+import SwiftUI
+
+@main
+struct KrokyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
